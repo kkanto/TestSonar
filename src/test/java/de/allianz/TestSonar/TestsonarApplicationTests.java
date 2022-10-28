4f088ec3cee12f8bb8fe9a96c9415f5bff947ac7
kkanto <108447856+kkanto@users.noreply.github.com>
2022-10-28 14:40:24 +0200

    Create maven project

---
+++ src/test/java/de/allianz/TestSonar/TestsonarApplicationTests.java
@@ -0,0 +1,13 @@
+package de.allianz.TestSonar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestsonarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
